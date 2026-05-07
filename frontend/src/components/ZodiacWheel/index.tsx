10285d9138d7c45bc30d8f461830de38ef1f12ac
Ed Dubnitsky <eddubnitsky@Eds-MacBook-Air.local>
2026-05-07 11:52:57 +0300

    refactor(ZodiacWheel): parameterize orbit and marker radius in PlanetMarkers

--- frontend/src/components/ZodiacWheel/index.tsx
+++ frontend/src/components/ZodiacWheel/index.tsx
@@ -264,6 +264,8 @@ export const ZodiacWheel: React.FC<ZodiacWheelProps> = ({
                   onClusterClick={handleClusterClick}
                   size={config.size}
                   chartRotation={rotationDeg}
+                  orbitRadius={config.size * 0.32}
+                  markerRadius={config.size * 0.015}
                 />
               )}
 
